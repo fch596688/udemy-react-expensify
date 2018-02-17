15a78ad32dc68feafe347e3ec266e367cd3019c3
fch596688 <fch596688@gmail.com>
2018-02-16 23:59:39 -0500

    dummy data from app.js

--- src/app.js
+++ src/app.js
@@ -13,15 +13,7 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount:4500 }));
-store.dispatch(addExpense({ description: "Gas bill", createdAt:1000 }));
-store.dispatch(addExpense({ description: "Gas bill", amount: 109500 }));
 
-
-const state = store.getState();
-console.log(state);
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={ store }>
